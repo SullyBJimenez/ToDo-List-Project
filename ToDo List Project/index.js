40416dcb802eb4addc7fdd3ced833586f0fd45a3
SullyBJimenez <101064015+SullyBJimenez@users.noreply.github.com>
2023-02-19 21:23:38 -0500

    Add files via upload

---
+++ ToDo List Project/index.js
@@ -0,0 +1,43 @@
+function App(){
+    const [todos, setTodos] = React.useState([
+        {
+            text: 'learn react',
+            isCompleted: false,
+        },
+        {
+            text: 'meet friend for lunch',
+            isCompleted: false,
+        },
+        {
+            text: 'build todo app',
+            isCompleted: false,
+        },
+        {
+            text: 'catch flight',
+            isCompleted: false,
+        }
+    ]);
+    
+    const addTodo = text => {
+        const newTodos = [...todos, {text:text, isCompleted:false}];
+        setTodos(newTodos);
+    }
+
+    const removeTodo = index => {
+        let temp = [...todos];
+        temp.splice(index,1);
+        setTodos(temp);
+    }
+    return (
+    <div className = "app">
+        {todos.map((todo,i) => 
+            <Todo index={i} key={i} todo={todo} remove={removeTodo}/>)}
+        <TodoForm addTodo={addTodo}/>
+    </div>
+    );
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
